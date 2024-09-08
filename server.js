1f202a40ef4de1985da5be5bf61afc8d47e53297
Carlos Mancini <carlos@devsquad.email>
2024-09-08 14:33:23 -0300

    Provinding a template so that the program answers to the questions as a programmer, and only responds to questions about coding.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const { OpenAI } = require('@langchain/openai');
+const { PromptTemplate } = require('@langchain/core/prompts');
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -15,9 +16,19 @@ const model = new OpenAI({
   model: 'gpt-3.5-turbo'
 });
 
+const prompt = new PromptTemplate({
+  template: "You are a programming expert and will answer the user's coding question as thoroughly as possible using JavaScript. If the question is unrelated to coding, do not answer. \n{question}",
+  inputVariables: ['question']
+});
+
 const promptFunc = async (input) => {
   try {
-    const res = await model.invoke(input);
+    // Format the prompt with the user input
+    const promptInput = await prompt.format({
+      question: input
+    });
+
+    const res = await model.invoke(promptInput);
     return res;
   }
   catch (err) {
